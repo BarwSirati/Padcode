694dff22d6b615e7f215ed18b4cf01656dd1c611
Apinyawat Khwanpruk <sanfong2222@gmail.com>
2022-04-11 16:50:30 +0700

    Remove comment

--- src/App.java
+++ src/App.java
@@ -1,7 +1,4 @@
 import javafx.application.Application;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Parent;
-// import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import ui.Padcode;
@@ -9,8 +6,6 @@ import ui.Padcode;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Parent root = FXMLLoader.load(getClass().getResource("/UI/Padcode.fxml"));
-        //Scene scene = new Scene(root);
         primaryStage.setScene(new Padcode().getScene());
         primaryStage.setTitle("Padcode");
         primaryStage.getIcons().add(new Image(App.class.getResourceAsStream("/asset/img/icon.png")));
